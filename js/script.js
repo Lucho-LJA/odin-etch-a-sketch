3987b2ef3fc5b39bd2673a125c5d689be769d401
Lucho-LJA <ljallauca.a@gmail.com>
2022-03-25 21:36:12 -0500

    add color event

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 //variables html
 const boardContainer = document.querySelector('#board-selector');
+const resetButton = document.querySelector('#reset');
+const clearButton = document.querySelector('#clear');
+const inputColor = document.querySelector('#input-color');
+const inputColums = document.querySelector('#input-colums');
+const inputRows = document.querySelector('#input-rows');
 
 //genral variables
 const boardWidth = 50;
@@ -35,10 +40,26 @@ function createGrid(nColums=16,nRows=16){
     cells.forEach(oneCell => oneCell.addEventListener("mouseover", changeColor));
 }
 
+//events
+inputColor.addEventListener('change', changeColorCell);
+inputColor.addEventListener('input', changeColorCell);
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    createGrid();
+});
+
+
+
 //event functions
+function changeColorCell(){
+    colorCell = this.value;
+    console.log(`changed color to ${colorCell}`)
+}
 function changeColor(){
     this.style.backgroundColor=colorCell;
-    console.log('events have been created');
+    console.log('color events have been created');
 }
-createGrid();
+
+
+
 
